432014e1df3a02a04a2126ec1bbe2da805ffcb08
hshaik <hshaik@progress.com>
2021-04-22 16:46:04 +0530

    Java Basics

--- JSRefresher/Basic.js
+++ JSRefresher/Basic.js
@@ -1,1 +1,30 @@
 /****   BASIC JS  *** */
+
+//CORE LANGUAGE FEATURES
+//VARIABLES AND FUNCTIONS
+/*************************
+ * 
+ * ##############  var ############################
+ * Any statement which starts with var is a global variable and can declare any number of times and its 
+ * value changes as the program moves forward
+ * 
+ * You can even redeclare var with same variable name as its used previously
+ * 
+ * ############### let ############################
+ * 
+ * You can declare the variable only once and scope is limited to block in which its declared or initialised
+ * Its very similiar to a local variable in java/other programming languages
+ * 
+ * ############# const ###########################
+ * 
+ * As the name itself suggests that its a constant and value cannot be altered
+ *
+ * 
+ */
+
+var name = 'Test';
+var age=44;
+var isFalse=false;
+var percent=33.5;
+
+var name='Test2';
